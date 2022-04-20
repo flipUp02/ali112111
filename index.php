e17e530b35f2659e1f140af05290a00517f58073
flipUp02 <80167896+flipUp02@users.noreply.github.com>
2022-04-20 02:57:00 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+	  <base target="_parent">
+	  <meta charset="UTF-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">    
+	  <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
+	  <meta name="robots" content="noindex, noimageindex, nofollow, nosnippet">
+	  <title>title here</title>
+	</head>
+	<body style="padding: 0;margin: 0;overflow: hidden;">
+	<iframe  scrolling="no" allowfullscreen='' frameborder='0' height='100%' src='https://jodyme.com/370/p/?p=s_ca2&o_id=1&s=47df2bfdddfc0bfd79034d97c3074f7f0e478ccc21d39d1446f9607fa447ca9827678b759c391e0f6ba185cf79670f2064b549395264d0bb930a1bff0322554e&id=1' style='border:0px #ffffff none;height: 100vh;width: 100vw;' width='100%'></iframe>
+	</body>
+	</html>
